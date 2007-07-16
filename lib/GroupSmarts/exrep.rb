a6d3a5b310b178d837994e40f9c9db89a8098608
cch1 <cch1@b5fa333d-9bf8-45b9-b2f2-2709d1971210>
2007-07-16 19:26:40 +0000

    Refactored ExRep to mimic previous hacks to XmlSerialize. Changed name of Hash module to avoid namespace collisions within AR.
    git-svn-id: svn://cho.hapgoods.com/groupsmarts/hashify/trunk@5 b5fa333d-9bf8-45b9-b2f2-2709d1971210

--- lib/GroupSmarts/exrep.rb
+++ lib/GroupSmarts/exrep.rb
@@ -7,8 +7,9 @@ module GroupSmarts
     def self.included(base)
       base.extend(ClassMethods)
       base.class_eval do
-        include Hash
-        include XML
+        include InstanceMethods
+        include SmartHash
+        include SmartXML
       end
       ::ActiveRecord::XmlSerializer.class_eval do
         include XmlSerializer
@@ -18,44 +19,87 @@ module GroupSmarts
     end  
 
     module ClassMethods
+      # enumerate the instance methods that collectively generate the external representation of self.
       def exrep_methods
-        [primary_key] + content_columns.map(&:name).sort
+        available = class_variable_defined?(:@@baseline_attributes)        
+        available ? class_variable_get(:@@baseline_attributes) : [primary_key] + content_columns.map(&:name).sort
       end
     end
     
-    module XML
-      def xto_xml(options)
-        serializer = XmlSerializer.new(self, options)
-        serializer.builder.tag!(self.class.to_s.underscore, root_tag_options) do
-          serializer.add_attributes
-          serializer.add_includes
-          serializer.add_procs
-          yield serializer.builder if block_given?
-        end
+    module InstanceMethods
+      # Generate a URL that represents self.
+      def exrep_url
+        default_url_options[:host] = MMConfig.app_url
+        method = self.class.to_s.underscore + '_url'
+        self.send(method, self)
       end
+    end
+    
+    module SmartXML
     end
     
     module XmlSerializer
+      # Add support for an href attribute of the root node via url method on serialzed object.
       def serialize_with_fu
-        print "serialize"
         args = [root]
         
         args << {:xmlns=>options[:namespace]} if options[:namespace]
         args << {:type=>options[:type]} if options[:type]
-        args << {:href => @record.url} if options[:href] and @record.respond_to?(:url)
-        print args
-  
+        args << {:href => @record.exrep_url} if options[:href] and @record.respond_to?(:exrep_url)
+
         builder.tag!(*args) do
-          add_attributes
-          add_includes
+          add_attributes_with_fu
+          add_includes_with_fu
           add_procs
           yield builder if block_given?
         end
-#        serialize_without_fu
       end
-    end
+      
+      def add_includes_with_fu
+        # Convert include to nested hash of options.
+        associations_hash = options[:include].is_a?(::Hash) ? options[:include] : Array(options[:include]).inject({}){|h,a| h[a] = {};h}
+        associations_hash.keys.each do |association|
+          propogating_options = options.reject{|k,v| !(%w(builder indent skip_instruct).include?(k.to_s))}
+          opts = associations_hash[association].merge(propogating_options)
+          case @record.class.reflect_on_association(association).macro
+          when :has_many, :has_and_belongs_to_many
+            records = @record.send(association)
+            tag = association.to_s
+            tag = tag.dasherize if dasherize?
+            if records.empty?
+              builder.tag!(tag, :type => :array)
+            else
+              builder.tag!(tag, :type => :array) do
+                association_name = association.to_s.singularize
+                records.each do |record| 
+                  o = opts.merge(:root => association_name, :type => (record.class.to_s.underscore == association_name ? nil : record.class.name))
+                  record.to_xml(o)
+                end
+              end
+            end
+          when :has_one, :belongs_to
+            if record = @record.send(association)
+              record.to_xml(opts.merge(:root => association))
+            end # if
+          end # case
+        end # do
+      end # def
+      
+      def add_attributes_with_fu
+        attribute_names = options[:methods] || (options[:enumerator] && @record.class.send(options[:enumerator])) || @record.class.respond_to?(:exrep_methods) && @record.class.exrep_methods|| @record.attribute_names
+        attribute_names = Array(attribute_names).map(&:to_s)
+        
+        attribute_names = attribute_names + Array(options[:with]).map(&:to_s)
+        attribute_names = attribute_names - Array(options[:without]).map(&:to_s)
     
-    module Hash
+        attribute_names.each do |name|
+          attribute_class = @record.attribute_names.include?(name) ? ::ActiveRecord::XmlSerializer::Attribute : ::ActiveRecord::XmlSerializer::MethodAttribute
+          add_tag(attribute_class.new(name, @record))
+        end
+      end
+    end # module
+    
+    module SmartHash
       def to_hash(options = {})
         hash = {}
     
@@ -69,7 +113,6 @@ module GroupSmarts
       def direct(options = {})
         method_names = options[:methods] || (options[:enumerator] && self.class.send(options[:enumerator])) || self.class.respond_to?(:exrep_methods) && self.class.exrep_methods|| self.attribute_names
         method_names = Array(method_names).map(&:to_s)
-    
         
         method_names = method_names + Array(options[:with]).map(&:to_s)
         method_names = method_names - Array(options[:without]).map(&:to_s)
@@ -83,7 +126,7 @@ module GroupSmarts
       def associations(options = {})
         hash = {}
         # Convert include to nested hash of options.
-        associations_hash = options[:include].is_a?(Hash) ? options[:include] : Array(options[:include]).inject({}){|h,a| h[a] = {};h}
+        associations_hash = options[:include].is_a?(::Hash) ? options[:include] : Array(options[:include]).inject({}){|h,a| h[a] = {};h}
         # Iterate over associations, calling to_hash.
         associations_hash.keys.each do |association|
           opts = associations_hash[association]
